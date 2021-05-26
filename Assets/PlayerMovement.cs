fd248b8bc9d301cf772c0773f16d4d13fd69ac84
Reinis <rlukins123@gmail.com>
2021-05-26 22:57:22 +0300

    Add temp following object, code cleanup

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,7 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform groundCheck;
+    public Transform Spider;
     public LayerMask groundMask;
+    AudioSource audio;
+    AudioClip footstep;
 
     public float movespeed = 12f;
     public float gravity = -9.81f;
@@ -16,7 +19,9 @@ public class PlayerMovement : MonoBehaviour
     bool isGround;
 
     void Start() {
-        
+        audio = GetComponent<AudioSource>();
+        controller = GetComponent<CharacterController>();
+        StartCoroutine(MyCoroutine(Spider));
     }
 
     void Update() {
@@ -35,12 +40,10 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
-        
         // Yump logic
         if(Input.GetKeyDown("space") && isGround) {
             velocity.y = Mathf.Sqrt(-6f * gravity);
         }
-
         // Sprint logic
         if(Input.GetKey(KeyCode.LeftShift)) {
             if(movespeed < 24f) {
@@ -48,6 +51,29 @@ public class PlayerMovement : MonoBehaviour
             }
         } else {
             movespeed = 12f;
+        }
+
+        // Footstep sound
+        if(isGround && Input.GetKey(KeyCode.W)) { 
+            PlaySound();
+        }
+    }
+
+    IEnumerator MyCoroutine(Transform Spider) {
+        while(Vector3.Distance(transform.position, Spider.position) > 0.2f) {
+            Spider.position = Vector3.Lerp(Spider.position, transform.position, 1f * Time.deltaTime);
+            Debug.Log(Vector3.Distance(transform.position, Spider.position));
+            yield return null;
+        }
+
+        Debug.Log("Reached");
+        yield return new WaitForSeconds(3f);
+        Debug.Log("Finished");
+    }
+
+    void PlaySound() {
+        if(!audio.isPlaying) {
+            audio.Play();
         }
     }
 }
